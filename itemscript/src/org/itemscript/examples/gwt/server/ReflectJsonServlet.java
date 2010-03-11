873023a89a598e3ccf6d9db1ef98e49d95663d9a
jacobjdavies <jacobjdavies@707d44fe-5d28-11de-8d7d-01d876f1962d>
2010-03-11 00:04:50 +0000

    Added some warnings about the ReflectJsonServlet.

--- itemscript/src/org/itemscript/examples/gwt/server/ReflectJsonServlet.java
+++ itemscript/src/org/itemscript/examples/gwt/server/ReflectJsonServlet.java
@@ -1,3 +1,31 @@
+/*
+ * Copyright © 2010, Data Base Architects, Inc. All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 
+ *     * Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright notice,
+ *       this list of conditions and the following disclaimer in the documentation
+ *       and/or other materials provided with the distribution.
+ *     * Neither the names of Kalinda Software, DBA Software, Data Base Architects, Itemscript
+ *       nor the names of its contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * 
+ * Author: Jacob Davies
+ */
 
 package org.itemscript.examples.gwt.server;
 
@@ -13,6 +41,18 @@ import org.itemscript.core.JsonSystem;
 import org.itemscript.core.values.JsonValue;
 import org.itemscript.standard.MinimalConfig;
 
+/**
+ * A testing class that simply parses and re-sends any JSON sent to this.
+ * 
+ * Note that this should ABSOLUTELY NOT be installed as-is on a webserver that is visible to the world! It may cause you to be
+ * vulnerable to cross-site scripting attacks or expose you to other major security problems.
+ * 
+ * Protecting it with authentication MAY protect you from some problems, but you should make very certain you understand
+ * what you're getting into before you do so.
+ * 
+ * @author Jacob Davies<br/><a href="mailto:jacob@itemscript.org">jacob@itemscript.org</a>
+ *
+ */
 public class ReflectJsonServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
